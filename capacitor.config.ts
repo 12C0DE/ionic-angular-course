3d42c40b04dad536db014be5013027e83e6a62be
Ruben Hernandez <rubenhnt@gmail.com>
2021-12-02 14:16:01 -0600

    creating android native app code

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ionic.angular.course.12h',
+  appName: 'ionic-angular-course',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
